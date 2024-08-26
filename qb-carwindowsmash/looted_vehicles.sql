fc13a7cd3b40caa1e3d832764227c6dfa806aa04
jackshiers13 <72285311+jackshiers13@users.noreply.github.com>
2024-08-27 00:59:44 +0100

    Delete qb-carwindowsmash directory

--- qb-carwindowsmash/looted_vehicles.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `looted_vehicles` (
-    `plate` varchar(8) NOT NULL,
-    `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`plate`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
